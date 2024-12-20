134b1cabe9711157f74bbf755b294e5c8c757b13
psychopr0f <nourmina.francis@gmail.com>
2024-12-20 14:13:43 +0200

    completed the project

---
+++ main.py
@@ -0,0 +1,38 @@
+def main():
+    book_path = "books/frankenstein.txt"
+    text = get_book_text(book_path)
+    num_words = get_num_words(text)
+    chars_dict = get_chars_dict(text)
+    print_report(text)
+
+def print_report(text):
+    chars_dict = get_chars_dict(text)
+    print("--- Begin report of books/frankenstein.txt ---")
+    print(f"{get_num_words(text)} words found in the document")
+    for char in chars_dict:
+        print(f"The '{char}' character was found {chars_dict[char]} times")
+    print("--- End report ---")
+
+
+def get_num_words(text):
+    words = text.split()
+    return len(words)
+
+
+def get_chars_dict(text):
+    chars = {}
+    for c in text:
+        lowered = c.lower()
+        if lowered in chars:
+            chars[lowered] += 1
+        else:
+            chars[lowered] = 1
+    return chars
+
+
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
+
+
+main()
